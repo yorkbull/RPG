54a1f57d0ad3eb28d4fee61a717d4e46fae4f359
Jounaedi <72859711+Jounaedi@users.noreply.github.com>
2020-12-12 14:59:51 +0100

    Add files via upload

---
+++ monstre.py
@@ -0,0 +1,24 @@
+
+
+#Monstre
+
+#Japon
+
+class Monstre1:
+    def init(self):
+        self.name = ""
+        self.hp = 0
+        self.mp = 0
+        self.status_effects = []
+        self.location = "start"
+
+Kodama = Monstre1()
+
+
+class Monstre2:
+    def init(self):
+        self.name = ""
+        self.hp = 0
+        self.mp = 0
+        self.status_effects = []
+        self.location = "start"
